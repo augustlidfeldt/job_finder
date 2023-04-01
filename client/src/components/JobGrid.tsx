d009d3509800abbafcbec8599fe5ba57eeb59fe7
August Lidfeldt <august.lidfeldt.4322@student.lu.se>
2023-04-01 19:06:37 +0200

    Added key to map function in JobGrid.tsx

--- client/src/components/JobGrid.tsx
+++ client/src/components/JobGrid.tsx
@@ -1,25 +1,20 @@
-import React, { ReactNode, useEffect, useState } from 'react'
-import {JobCard} from './JobCard'
+import React, { ReactNode, useEffect, useState } from "react";
+import { JobCard } from "./JobCard";
 
-export const JobGrid:React.FC = () => {
-    const [jobs, setJobs] = useState([]);
+export const JobGrid: React.FC = () => {
+  const [jobs, setJobs] = useState([]);
 
-    useEffect(()=>{
-        async function getJobs() {
-            const res = await fetch('http://localhost:5000/jobs/detailed');
-            const data = await res.json();
-            setJobs(data);
-        }
-        getJobs();
-    }, [])
-
-    function mapJobs():any {
-
-        return jobs.map((job)=><JobCard job={job} ></JobCard>)    
+  useEffect(() => {
+    async function getJobs() {
+      const res = await fetch("http://localhost:5000/jobs/detailed");
+      const data = await res.json();
+      setJobs(data);
     }
-  return (
-    <div className='jobgrid'>
-        {mapJobs()}
-    </div>
-  )
-}
+    getJobs();
+  }, []);
+
+  function mapJobs(): any {
+    return jobs.map((job, i) => <JobCard key={i} job={job}></JobCard>);
+  }
+  return <div className="jobgrid">{mapJobs()}</div>;
+};
